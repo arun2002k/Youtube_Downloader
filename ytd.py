9fe9aeae179a468af06bf94b6ff50f19b71b539b
Arun <85438554+arun2002k@users.noreply.github.com>
2023-11-14 10:34:59 +0530

    Update ytd.py

--- ytd.py
+++ ytd.py
@@ -1,7 +1,13 @@
+# Importing python package 
 import pytube
 
-video_url = input("Wreite your video url : ")
+# Paste the youtube video url 
+video_url = input("Write your video url : ")
 yt = pytube.YouTube(video_url)
+
+# Getting the video in HD format
 stream = yt.streams.get_highest_resolution()
+
+# Video is downloading and stored in your pc
 print("Video is downloading....")
 stream.download()
